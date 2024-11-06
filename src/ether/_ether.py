eeac8db905ef677084e8e9c145819ebda03e29bb
shababo <benshababo@gmail.com>
2024-11-06 14:03:00 -0800

    delete old ether run stuff

--- src/ether/_ether.py
+++ src/ether/_ether.py
@@ -232,11 +232,6 @@ def add_ether_functionality(cls):
             else:
                 self._logger.debug(f"Received message for unknown topic: {topic}")
 
-    # @staticmethod
-    # def _ether_run(*args, **kwargs):
-    #     """Run an instance of this class with the given args and kwargs"""
-    #     instance = cls(*args, **kwargs)
-    #     instance.run()
     
     # Add run method
     def run(self):
@@ -277,7 +272,6 @@ def add_ether_functionality(cls):
     cls.run = run
     cls.cleanup = cleanup
     cls.receive_single_message = receive_single_message
-    # cls._ether_run = _ether_run
 
     
     # Modify __init__ to initialize attributes
