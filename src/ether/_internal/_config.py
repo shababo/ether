cd9d0a3799af7472f270830e0933d9a68e1558ed
shababo <benshababo@gmail.com>
2025-01-20 16:37:55 -0800

    update discovery ports to be in range, duh

--- src/ether/_internal/_config.py
+++ src/ether/_internal/_config.py
@@ -76,8 +76,8 @@ class EtherNetworkConfig(BaseModel):
     reqrep_backend_port: int = 5560
     redis_host: str = "0.0.0.0"  # Add separate Redis host config
     redis_port: int = 6379
-    session_discovery_port: int = 301309
-    session_query_port: int = 301310
+    session_discovery_port: int = 31309
+    session_query_port: int = 31310
 
 
 class EtherConfig(BaseModel):
